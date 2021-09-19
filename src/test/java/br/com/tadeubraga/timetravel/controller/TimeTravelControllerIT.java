8d5db9a0257bd081a2ef5ecd1ad3b84d94b95c15
Tadeu Braga <tadeufala@gmail.com>
2021-09-19 18:47:45 -0300

    Finished impl of TimeTravelController Integration Tests.

--- src/test/java/br/com/tadeubraga/timetravel/controller/TimeTravelControllerIT.java
+++ src/test/java/br/com/tadeubraga/timetravel/controller/TimeTravelControllerIT.java
@@ -1,13 +1,17 @@
 package br.com.tadeubraga.timetravel.controller;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +26,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 
 import br.com.tadeubraga.timetravel.controller.dto.PlaceDto;
 import br.com.tadeubraga.timetravel.controller.dto.TimeTravelDto;
@@ -45,24 +50,33 @@ class TimeTravelControllerIT {
 	}
 
 	Calendar getZeroHourCalendarInstance() {
-		Calendar result = Calendar.getInstance();
-		result.set(Calendar.HOUR, 0);
+		TimeZone zone = TimeZone.getTimeZone("UTC");
+		Calendar result = Calendar.getInstance(zone);
+		result.set(Calendar.HOUR_OF_DAY, 0);
 		result.set(Calendar.MINUTE, 0);
 		result.set(Calendar.SECOND, 0);
 		result.set(Calendar.MILLISECOND, 0);
-		result.set(Calendar.DST_OFFSET, 0);
 		return result;
+	}
+
+	public TimeTravelDto getTimeTravelDto() {
+		var personalGalaticId = "nick76";
+		var date = getZeroHourCalendarInstance();
+		var place = PlaceDto.builder().country("England").city("London").build();
+		return TimeTravelDto.builder().personalGalaticId(personalGalaticId).date(date).place(place).build();
+	}
+
+	public TimeTravel getTimeTravel() throws JsonMappingException, JsonProcessingException {
+		TimeTravel timeTravel = getTimeTravelDto().toModel();
+		timeTravel.setId(1L);
+		timeTravel.getPlace().setId(1L);
+		return timeTravel;
 	}
 
 	@Test
 	void should_Save_When_AValidTimeTravelIsGiven() throws Exception {
-		var personalGalaticId = "nick76";
-		var date = getZeroHourCalendarInstance();
-		var place = PlaceDto.builder().country("England").city("London").build();
-		TimeTravelDto t = TimeTravelDto.builder().personalGalaticId(personalGalaticId).date(date).place(place).build();
-		TimeTravel mockedResult = clone(t).toModel();
-		mockedResult.setId(1L);
-		mockedResult.getPlace().setId(1L);
+		TimeTravelDto t = getTimeTravelDto();
+		TimeTravel mockedResult = getTimeTravel();
 		when(timeTravelService.save(any(TimeTravel.class))).thenReturn(mockedResult);
 
 		MvcResult mvcResult = mockMvc.perform(post("/time-travels").contentType(MediaType.APPLICATION_JSON)
@@ -72,4 +86,28 @@ class TimeTravelControllerIT {
 		assertThat(result, equalTo(mockedResult));
 	}
 
+	@Test
+	void should_FindAll_When_MethodIsCalled() throws Exception {
+		var timeTravel = getTimeTravel();
+		when(timeTravelService.findById(1L)).thenReturn(timeTravel);
+
+		MvcResult mvcResult = mockMvc.perform(get("/time-travels/1").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is(200)).andReturn();
+		var result = OBJECT_MAPPER.readValue(mvcResult.getResponse().getContentAsString(), TimeTravelDto.class);
+
+		assertThat(result, equalTo(TimeTravelDto.ofModel(timeTravel)));
+	}
+
+	@Test
+	void should_FindById_When_MethodIsCalled() throws Exception {
+		var timeTravel = getTimeTravel();
+		when(timeTravelService.findAll()).thenReturn(List.of(timeTravel));
+
+		MvcResult mvcResult = mockMvc.perform(get("/time-travels/").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is(200)).andReturn();
+		CollectionType listType = OBJECT_MAPPER.getTypeFactory().constructCollectionType(ArrayList.class, TimeTravelDto.class);
+		List<TimeTravelDto> result = OBJECT_MAPPER.readValue(mvcResult.getResponse().getContentAsString(), listType);
+
+		assertThat(result.get(0), equalTo(TimeTravelDto.ofModel(timeTravel)));
+	}
 }
